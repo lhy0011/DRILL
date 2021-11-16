94c324d5bb9cf6d4bfc1ab55c5f543435a5b0580
lhy0011 <90214737+lhy0011@users.noreply.github.com>
2021-11-16 16:07:00 +0900

    .

---
+++ 12/bird.py
@@ -0,0 +1,40 @@
+from pico2d import *
+import random
+import game_framework
+
+PIXEL_PER_METER = (10.0 / 0.3)
+FLY_SPEED_KMPH = 20.0
+FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
+FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+class Bird:
+    image = None
+    def __init__(self):
+        if Bird.image == None:
+            Bird.image = load_image('bird100x100x14.png')
+        self.x, self.y = random.randint(100, 1500), random.randint(300, 500)
+        self.velocity = FLY_SPEED_PPS
+        self.dir = random.randint(-1, 1)
+        self.frame = 0
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
+        if self.dir == 0:
+            self.dir = 1
+        if self.x > 1550:
+            self. dir = -1
+        elif self.x < 50:
+            self.dir = 1
+        self.x += self.velocity * game_framework.frame_time * self.dir
+
+    def draw(self):
+        if self.dir == 1:
+            Bird.image.clip_draw(int(self.frame) * 100, 0, 100, 100, self.x, self.y)
+        else:
+            Bird.image.clip_composite_draw(int(self.frame) * 100, 0, 100, 100, 0, 'h', self.x, self.y, 100, 100)
